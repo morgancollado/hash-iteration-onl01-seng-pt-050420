8433303c37975d5b47f984a43ba50ff26936a4c3
Morgan Collado <morgan.collado@gmail.com>
2020-05-14 01:04:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ def age_appropriate_birthday(birthday_kids)
     if age < 10 
        puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
      else age >10
-       "You are too old for this."
+       puts "You are too old for this."
      end 
    end 
 end 
